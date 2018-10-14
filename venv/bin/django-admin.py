4a5ad3bf9c7aab3f3c39255869ed7d9f60a70b5f
Aaron Green <aarongg2@illinois.edu>
2018-10-14 16:52:11 -0500

    heroku django

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/AaronGreen/Programming_All_Workspace/CS411/safetravels/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
