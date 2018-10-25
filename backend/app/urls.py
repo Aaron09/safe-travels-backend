62d662c1e143e66b75b336a3f69659fe071a4665
Aaron Green <aarongg2@illinois.edu>
2018-10-25 00:33:03 -0500

    basic county info endpoint

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf.urls import url
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'', include('app.urls')),
+    url(r'^county/(?P<county_id>\d+)/$', views.county_information, name='county_information'),
+    url(r'^review/all/(?P<county_id>\d+)/$', views.county_reviews, name='county_reviews'),
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
+from app.models import County
+import json
 
-# Create your views here.
+def county_information(request, county_id):
+    query = f"select * from app_county where id={county_id}"
+    result = County.objects.raw(query)
+    serialized_result = serializers.serialize("json", result, fields=('name', 'state', 'population'))
+    raw_county_data = json.loads(serialized_result)[0]
+    return JsonResponse(raw_county_data["fields"])
