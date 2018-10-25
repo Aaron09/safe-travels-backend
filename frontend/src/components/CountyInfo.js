43688e5c1280782146cd5829ff057243465fb552
Angad Garg <angad.garg3@gmail.com>
2018-10-25 00:33:45 -0500

    frontend county specific

---
+++ frontend/src/components/CountyInfo.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react"
+import axios from 'axios'
+
+const backend_url = "http://localhost:8000"
+
+class CountyInfo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { loaded: false }
+  }
+
+  componentDidMount() {
+    axios.get(backend_url + "/county?" + this.props.id).then(response => {
+      this.setState({
+        name: response.data.name,
+        state: response.data.state,
+        population: response.data.population,
+        loaded: true        
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.loaded ? (
+          <div>
+            <div>Name: {this.state.name}</div>
+            <div>State: {this.state.state}</div>
+            <div>Population: {this.state.population}</div>
+          </div>
+        ) : (
+          <div>None</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default CountyInfo;
